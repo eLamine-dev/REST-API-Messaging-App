f8b2efc245ee08ef6f4f886d84f6d3b8e4decf72
e-lamine <111455099+eLamine-dev@users.noreply.github.com>
2025-01-22 15:02:28 +0100

    test from wi pc

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const conversationRoutes = require('./routes/conversationRoutes');
-const friendRoutes = require('./routes/friendRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const conversationRoutes = require("./routes/conversationRoutes");
+const friendRoutes = require("./routes/friendRoutes");
 
 const app = express();
-app.use(
-   cors({
-      origin: 'http://localhost:5173',
-      credentials: true,
-   })
-);
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/conversations', conversationRoutes);
-app.use('/api/friends', friendRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/conversations", conversationRoutes);
+app.use("/api/friends", friendRoutes);
 
 module.exports = app;
--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,88 +1,88 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const prisma = require('../utils/prismaClient');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const prisma = require("../utils/prismaClient");
 
 dotenv.config();
 
 exports.register = async (req, res) => {
-   const { email, password, username } = req.body;
-   const hashedPassword = await bcrypt.hash(password, 10);
+  const { email, password, username } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-   try {
-      const user = await prisma.user.create({
-         data: { email, password: hashedPassword, username },
-      });
-      res.status(201).json({ user });
-   } catch (error) {
-      res.status(400).json({ error: 'Registration Error.' });
-   }
+  try {
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword, username },
+    });
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(400).json({ error: "Registration Error." });
+  }
 };
 
 exports.login = async (req, res) => {
-   const { email, password } = req.body;
+  const { email, password } = req.body;
 
-   try {
-      const user = await prisma.user.findUnique({ where: { email } });
-      if (!user) {
-         console.log('Invalid user.');
-         return res.status(400).json({ error: 'Invalid user.' });
-      }
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      console.log("Invalid user.");
+      return res.status(400).json({ error: "Invalid user." });
+    }
 
-      const valid = await bcrypt.compare(password, user.password);
-      if (!valid) {
-         return res.status(400).json({ error: 'Invalid password.' });
-      }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return res.status(400).json({ error: "Invalid password." });
+    }
 
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-         expiresIn: '2h',
-      });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
 
-      await prisma.user.update({
-         where: { id: user.id },
-         data: { status: 'ONLINE' },
-      });
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { status: "ONLINE" },
+    });
 
-      res.json({ token, user: { id: user.id, username: user.username } });
-   } catch (error) {
-      res.status(500).json({ error: 'Error during login.' });
-   }
+    res.json({ token, user: { id: user.id, username: user.username } });
+  } catch (error) {
+    res.status(500).json({ error: "Error during login." });
+  }
 };
 
 exports.logout = async (req, res) => {
-   try {
-      await prisma.user.update({
-         where: { id: req.user.userId },
-         data: { status: 'OFFLINE' },
-      });
+  try {
+    await prisma.user.update({
+      where: { id: req.user.userId },
+      data: { status: "OFFLINE" },
+    });
 
-      const user = await prisma.user.findUnique({
-         where: { id: req.user.userId },
-      });
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.userId },
+    });
 
-      console.log('User logged out:', user.status);
+    console.log("User logged out:", user.status);
 
-      res.clearCookie('token');
-      res.json({ message: 'Logged out successfully.' });
-   } catch (error) {
-      res.status(500).json({ error: 'Error during logout.' });
-   }
+    res.clearCookie("token");
+    res.json({ message: "Logged out successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error during logout." });
+  }
 };
 
 exports.validateToken = async (req, res) => {
-   try {
-      const user = await prisma.user.findUnique({
-         where: { id: req.user.userId },
-         select: { id: true, username: true, email: true },
-      });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.userId },
+      select: { id: true, username: true, email: true },
+    });
 
-      if (!user) {
-         return res.status(401).json({ error: 'Invalid token.' });
-      }
+    if (!user) {
+      return res.status(401).json({ error: "Invalid token." });
+    }
 
-      res.json({ user });
-   } catch (error) {
-      console.error('Error validating token:', error);
-      res.status(500).json({ error: 'Internal server error.' });
-   }
+    res.json({ user });
+  } catch (error) {
+    console.error("Error validating token:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
 };
